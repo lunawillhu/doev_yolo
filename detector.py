c12d81386a707cecf5867fed872a6b907cf025c5
Usuario <luwillhuber@gmail.com>
2026-08-14 13:21:27 -0300

    Agregando funcion para procesar los frame

--- detector.py
+++ detector.py
@@ -4,9 +4,11 @@ from ultralytics import YOLO
 # yolo26n.pt
 
 # Camara de YOLO
-class detector(cam,mod, bucle):
-    def __init__(self, modelo, fuent_vid, umb_conf):
-        self.modelo = modelo
+class Detector:
+    
+    def __init__(self, modelo, fuent_vid, umb_conf, duracion):
+        self.modelo = YOLO(modelo)
         self.fuent_vid = fuent_vid
         self.umb_conf = umb_conf
+        self.duracion = duracion
 
